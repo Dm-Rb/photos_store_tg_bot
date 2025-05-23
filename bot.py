bae12e4900f3400e3237b2bed735f121a20c9ad1
Dm-Rb <dm.rb.418http@gmail.com>
2025-05-23 17:42:56 +0300

    добавлена опция загрузки файлов в режиме документа

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from config import config
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
-from handlers import cmd_start, cmd_new, cmd_show, cmd_edit, cmd_stop
+from handlers import cmd_start, cmd_new, cmd_show, cmd_edit, cmd_stop, cmd_help
 from middlewares.ban import BanMiddleware  # Импортируем наш middleware
 from callbacks import callback_handles
 from text.messages import msgs_cmd
@@ -23,8 +23,10 @@ async def main():
     dp.include_router(cmd_new.router)
     dp.include_router(cmd_show.router)
     dp.include_router(cmd_edit.router)
+    dp.include_router(cmd_help.router)
     dp.include_router(callback_handles.router)
     dp.include_router(cmd_stop.router)
+
 
     dp.update.middleware(BanMiddleware())  # This connected middleware contains the logic for ignoring banned users
 
@@ -33,6 +35,7 @@ async def main():
         BotCommand(command="show", description=msgs_cmd['show']),
         BotCommand(command="new", description=msgs_cmd['new']),
         BotCommand(command="edit", description=msgs_cmd['edit']),
+        BotCommand(command="help", description=msgs_cmd['help']),
         BotCommand(command="start", description=msgs_cmd['start']),
         BotCommand(command="stop", description=msgs_cmd['stop'])
     ])
